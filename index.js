9f250cdce1a3ddf133ff9f11f59852f195be2a13
rakibul114 <rakibulislam114@gmail.com>
2022-05-13 22:23:28 +0600

    created a post api to add a new booking

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const res = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,16 +25,27 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // connect to services data
+      // collection
         await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
+      const serviceCollection = client.db('doctors_portal').collection('services');
+      const bookingCollection = client.db('doctors_portal').collection('bookings');
+      
 
-        // get data from database
+
+        // get all data from database
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+      
+      // POST to add a new booking
+      app.post('/booking', async (req, res) => {
+        const booking = req.body;
+      });
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     }
     finally {
         
